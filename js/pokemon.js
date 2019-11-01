62c37fb29f7badfcbadedd7050835fd007fe5ad5
Coby Yates <cobylyates@gmail.com>
2019-10-31 22:19:57 -0600

    Got images to work

--- js/pokemon.js
+++ js/pokemon.js
@@ -12,7 +12,7 @@ async function getAPIData(url) {
 }
 
 // now, use the returned async data
-const theData = getAPIData("https://pokeapi.co/api/v2/pokemon/")
+const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
 .then(data => {
       for (const pokemon of data.results) {
         getAPIData(pokemon.url)
@@ -25,46 +25,47 @@ const theData = getAPIData("https://pokeapi.co/api/v2/pokemon/")
 
 let mainArea = document.querySelector("main");
 
-function getPokeNumber(charURL) {
+function getPokeNumber(id) {
   if(id < 10) return `00${id}`
   if(id > 9 && id < 100) {
     return `0${id}`
   } else return id
 }
 
-// var card = document.querySelector('.card');
-//   card.addEventListener('click', function() {
-//     card.classList.toggle('is-flipped');
-// });
+var card = document.querySelector('.card');
+  card.addEventListener('click', function() {
+    card.classList.toggle('is-flipped');
+});
 
 function populateDOM(single_pokemon) {
     let pokeScene = document.createElement("div")
-    let cardDiv = document.createElement("div");
+    let pokeDiv = document.createElement("div");
     let pokeFDiv = document.createElement("div");
     let pokeBDiv = document.createElement("div");
     let name = document.createElement("h3");
-    // let height = document.createElement("p");
     let pic = document.createElement("img");
 
-    // let pokeNum = getCharNumber(single_pokemon.url);
-    /* pic.setAttribute("class", "photo");
-      pokeFDiv.setAttribute("class", "card");*/
-    cardDiv.className = "card";
-    pokeFDiv.className = "card__face card__face--front";
-    pokeBDiv.className = "card__face card__face--back";
+    pokeScene.setAttribute('class', 'scene')
+    pokeDiv.setAttribute('class', 'card')
+    pokeFDiv.setAttribute('class', 'card__face card__face--front')
+    pokeBDiv.setAttribute('class', 'card__face card__face--back')
+    pic.setAttribute('class', 'picDivs')
 
     let pokeNum = getPokeNumber(single_pokemon.id)
 
     name.textContent = `${single_pokemon.name}`
-    height.textContent = single_pokemon.height
-    // pic.src = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${charNum}.png`;
-    pic.src = `../assets/images/00${pokeNum.id}.png`
 
-    cardDiv.appendChild(pokeBDiv);
-    pokeFDiv.appendChild(pic);
-    cardDiv.appendChild(pokeFDiv);
-    pokeBDiv.appendChild(name);
-    pokeBDiv.appendChild(height);
+    pic.src = `../assets/images/${pokeNum}.png`
 
-    mainArea.appendChild(pokeScene);
+    pokeFDiv.appendChild(pic)
+    pokeFDiv.appendChild(name)
+
+    // pokeBDiv.appendChild(height)
+    // pokeBDiv.appendChild(height)
+
+    pokeDiv.appendChild(pokeFDiv)
+    pokeDiv.appendChild(pokeBDiv)
+    pokeScene.appendChild(pokeDiv)
+
+    mainArea.appendChild(pokeScene)
 }
