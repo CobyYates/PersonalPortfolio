a5facf3759b1936a0e5a00b171b7953750303818
Coby Yates <cobylyates@gmail.com>
2019-11-01 12:45:58 -0600

    Added card effect, added a few key values

--- js/pokemon.js
+++ js/pokemon.js
@@ -4,8 +4,6 @@ async function getAPIData(url) {
     const response = await fetch(url);
     const data = await response.json();
     return data;
-    // console.log(data.name)
-    // console.log(data)
   } catch (error) {
     console.error(error);
   }
@@ -23,6 +21,11 @@ const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
   }
 );
 
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
 let mainArea = document.querySelector("main");
 
 function getPokeNumber(id) {
@@ -32,40 +35,68 @@ function getPokeNumber(id) {
   } else return id
 }
 
-var card = document.querySelector('.card');
-  card.addEventListener('click', function() {
-    card.classList.toggle('is-flipped');
-});
+/*function getPowers(arr) {
+  for (let i = 0; i < array.length; i++) {
+    let arrs = arr[i]    
+  }
+}*/
 
 function populateDOM(single_pokemon) {
     let pokeScene = document.createElement("div")
     let pokeDiv = document.createElement("div");
-    let pokeFDiv = document.createElement("div");
-    let pokeBDiv = document.createElement("div");
+    let pokeFront = document.createElement("div");
+    let pokeBack = document.createElement("div");
     let name = document.createElement("h3");
     let pic = document.createElement("img");
+    let powers = document.createElement("p")
+    let height = document.createElement("p")
+    let weight = document.createElement("p")
+    let pokeId = document.createElement("p")
+    let forms = document.createElement("p")
 
     pokeScene.setAttribute('class', 'scene')
     pokeDiv.setAttribute('class', 'card')
-    pokeFDiv.setAttribute('class', 'card__face card__face--front')
-    pokeBDiv.setAttribute('class', 'card__face card__face--back')
+    pokeFront.setAttribute('class', 'card__face card__face--front')
+    pokeBack.setAttribute('class', 'card__face card__face--back')
     pic.setAttribute('class', 'picDivs')
+    /*height.setAttribute('class', 'details')
+    weight.setAttribute('class', 'details')
+    powers.setAttribute('class', 'details')*/
 
     let pokeNum = getPokeNumber(single_pokemon.id)
 
-    name.textContent = `${single_pokemon.name}`
+    name.textContent = capitalize(`${single_pokemon.name}`)
+    height.textContent = `Height: ${single_pokemon.height}`
+    powers.textContent = `Abilities: ${single_pokemon.abilities[0].ability.name}`
+    weight.textContent = `Weight: ${single_pokemon.weight}`
+    pokeId.textContent = `ID: ${single_pokemon.id}`
+    forms.textContent = `Forms: ${capitalize(single_pokemon.forms[0].name)}`
+
+    console.log(forms)
 
     pic.src = `../assets/images/${pokeNum}.png`
 
-    pokeFDiv.appendChild(pic)
-    pokeFDiv.appendChild(name)
+    pokeFront.appendChild(pic)
+    pokeFront.appendChild(name)
+    pokeFront.appendChild(forms)
+    pokeFront.appendChild(pokeId)
 
-    // pokeBDiv.appendChild(height)
-    // pokeBDiv.appendChild(height)
+    pokeBack.appendChild(powers)
+    pokeBack.appendChild(height)
+    pokeBack.appendChild(weight)
 
-    pokeDiv.appendChild(pokeFDiv)
-    pokeDiv.appendChild(pokeBDiv)
+    pokeDiv.appendChild(pokeFront)
+    pokeDiv.appendChild(pokeBack)
     pokeScene.appendChild(pokeDiv)
 
     mainArea.appendChild(pokeScene)
+
+  pokeDiv.addEventListener('click', function() {
+    pokeDiv.classList.toggle('is-flipped');
+  });
 }
+
+
+/* Add multiple abilities
+Figure out best colors and designs for cards
+*/
