01a53f0ba2364bf24fbd71ba9e70735890c5a3ab
cobylyates <cobylyates@gmail.com>
2019-10-31 20:14:28 -0600

    Added more to pokemon

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,66 +1,70 @@
 // Reusable async function to fetch data from url param in the function call
-async function getPokemonData(url) {
-    try {
-    const response = await fetch(url)
-    const data = await response.json()
+async function getAPIData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
     // console.log(data.name)
-    console.log(data)
-    populateDOM(data.results)
-    } catch(error) {
-        console.error(error)
-    }
+    // console.log(data)
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-const data = getPokemonData('https://pokeapi.co/api/v2/pokemon/')
-
-console.log(data)
+// now, use the returned async data
+const theData = getAPIData("https://pokeapi.co/api/v2/pokemon/")
+.then(data => {
+      for (const pokemon of data.results) {
+        getAPIData(pokemon.url)
+        .then(pokeData => {
+          populateDOM(pokeData)
+        })
+      }
+  }
+);
 
 let mainArea = document.querySelector("main");
 
-function getCharNumber(charURL) {
-    let end = charURL.lastIndexOf("/");
-    let charID = charURL.substring(end - 2, end);
-    if (charID.indexOf("/") !== -1) {
-      return `00${charID.slice(1, 2)}`
-    } else {
-      return `0${charID}`;
-    }
-  }
+function getPokeNumber(charURL) {
+  if(id < 10) return `00${id}`
+  if(id > 9 && id < 100) {
+    return `0${id}`
+  } else return id
+}
 
 // var card = document.querySelector('.card');
 //   card.addEventListener('click', function() {
 //     card.classList.toggle('is-flipped');
 // });
 
+function populateDOM(single_pokemon) {
+    let pokeScene = document.createElement("div")
+    let cardDiv = document.createElement("div");
+    let pokeFDiv = document.createElement("div");
+    let pokeBDiv = document.createElement("div");
+    let name = document.createElement("h3");
+    // let height = document.createElement("p");
+    let pic = document.createElement("img");
 
-function populateDOM(pokeArray) {
-    pokeArray.forEach(pokemon => {
-      let cardDiv = document.createElement("div")
-      let pokeFDiv = document.createElement("div");
-      let pokeBDiv = document.createElement("div")
-      let name = document.createElement("h3")
-      let pic = document.createElement("img");
-
-      let charNum = getCharNumber(pokemon.url);
-      /* pic.setAttribute("class", "photo");
+    // let pokeNum = getCharNumber(single_pokemon.url);
+    /* pic.setAttribute("class", "photo");
       pokeFDiv.setAttribute("class", "card");*/
-      cardDiv.className = "card"
-      pokeFDiv.className = "card__face card__face--front"
-      pokeBDiv.className = "card__face card__face--back"
+    cardDiv.className = "card";
+    pokeFDiv.className = "card__face card__face--front";
+    pokeBDiv.className = "card__face card__face--back";
 
-      name.textContent = pokemon.name
-      pic.src = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${charNum}.png`
+    let pokeNum = getPokeNumber(single_pokemon.id)
 
-      cardDiv.appendChild(pokeBDiv)      
-      pokeFDiv.appendChild(pic);
-      cardDiv.appendChild(pokeFDiv)
-      pokeBDiv.appendChild(name);
+    name.textContent = `${single_pokemon.name}`
+    height.textContent = single_pokemon.height
+    // pic.src = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${charNum}.png`;
+    pic.src = `../assets/images/00${pokeNum.id}.png`
 
-      mainArea.appendChild(cardDiv);
-    });
+    cardDiv.appendChild(pokeBDiv);
+    pokeFDiv.appendChild(pic);
+    cardDiv.appendChild(pokeFDiv);
+    pokeBDiv.appendChild(name);
+    pokeBDiv.appendChild(height);
+
+    mainArea.appendChild(pokeScene);
 }
-
-var card = document.querySelector('.card');
-  card.addEventListener('click', function() {
-    card.classList.toggle('is-flipped');
-});
